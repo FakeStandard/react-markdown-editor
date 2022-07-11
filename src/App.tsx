7605985b62a0ac39e5b2d9e0a04fc022d6d61480
Riley Lin <rileylin.space@gmail.com>
2022-07-11 17:07:02 +0800

    set the basic Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header theme='light' />
-      <Main />
+      <Main theme='dark' />
       <Footer />
     </div>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/main.css'
 import Editor from "./Editor";
 import Preview from "./Preview";
 
-const Main = () => {
+interface IProps {
+    theme: string
+}
+
+const Main: React.FC<IProps> = ({ theme }) => {
+    const [content, setContent] = useState<string>(
+        `
+        # H1
+        ## H2
+        ### H3
+        #### H4
+        ##### H5
+
+        __bold__
+        **bold**
+        _italic_
+        `
+    )
+
     return (
         <main className="main">
             <Editor />
