ae1ffe31f0611d3fe0b511b67f9630984a28b76b
Riley Lin <rileylin.space@gmail.com>
2022-07-11 16:53:53 +0800

    set the basic Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Main from './components/Main';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header theme='light' />
       <Main />
       <Footer />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import '../styles/header.css'
+import { FiSun } from 'react-icons/fi'
+import { FaMoon } from 'react-icons/fa'
 
-const Header = () => {
+interface IProps {
+    theme: string
+    // toggleTheme: () => void
+}
+
+const Header: React.FC<IProps> = ({ theme }) => {
+    const headerStyle = `header ${theme}`
+
     return (
-        <header className="header">
-            header
+        <header className={headerStyle}>
+            <div className="header-title">
+                Markdown Editor
+            </div>
+            <div className="header-content">
+                {theme === 'dark' ? <FaMoon /> : <FiSun />}
+            </div>
         </header>
     )
 }
