1839aa8fcc135ab03b6c10ff879261518f468486
Riley Lin <rileylin.space@gmail.com>
2022-07-12 18:00:46 +0800

    adjust something...

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -11,7 +11,7 @@ interface IProps {
 const Preview: React.FC<IProps> = ({ theme, content }) => {
     const styles = `content ${theme}`
     const formattedContent = marked.parse(content)
-    console.log(formattedContent)
+    
     return (
         <div className="preview column">
             <h2 className="h2">Preview</h2>
